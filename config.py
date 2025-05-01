89a9e9e95245a6faa58ecf2096e435967e990047
ArifMustafaKhan <98647115+ArifMustafaKhan@users.noreply.github.com>
2025-05-01 11:49:26 +0530

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7991495621]
+api_id = "21600817"
+api_hash = "6d365a32f38717f82c9e77099f9fef78"
+bot_token = "7782951065:AAENXPxkbEX_yInMl2H3iruu1sEeFeAQMYw"
+auth_users = [6126740740]
