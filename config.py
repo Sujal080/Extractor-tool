aac0c77a491f0e84c767bcdf7b50f485e20bfbe8
Arif Mustafa Khan <98647115+ArifMustafaKhan@users.noreply.github.com>
2025-05-03 12:46:05 +0530

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "21600817"
-api_hash = "6d365a32f38717f82c9e77099f9fef78"
-bot_token = "7782951065:AAENXPxkbEX_yInMl2H3iruu1sEeFeAQMYw"
-auth_users = [6126740740]
+api_id = "enter your api id here"
+api_hash = "enter your api hash here"
+bot_token = "enter your bot token here"
+auth_users = [enter your auth user id here]
