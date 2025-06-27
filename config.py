7b9c948e9bee189533e5b0877f9e6c380dceee57
SUJAL CHAVDA <chavdasujal08@gmail.com>
2025-06-27 23:00:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api id here"
-api_hash = "enter your api hash here"
-bot_token = "enter your bot token here"
-auth_users = [enter your auth user id here]
+api_id = "25933223"
+api_hash = "6ef5a426d85b7f01562a41e6416791d3"
+bot_token = "8087125389:AAFQm9W73dxk2H0rzZe87CSMQRijGbPfmU8"
+auth_users = [8118667253]
